83eaf64b09c64e9085dc14720686918b8c7a743f
Resul Bülbül <resulbulbul61@hotmail.com>
2025-03-11 02:53:52 +0300

    Add project files.

---
+++ 02_Variables/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Klavyeden Veri Girişleri String Değişkenler
+
+            //Console.WriteLine("**** CSharp Hava Yolları Yolcu Bilgisi *****");
+            //Console.WriteLine();
+
+            //string passengerName, passengerSurname, passengerDistrict, passengerCity, passengerAge, passengerIdentityNumber;
+
+            //Console.Write("Yolcu Adı: ");
+            //passengerName = Console.ReadLine();
+
+            //Console.Write("Yolcu Soyadı: ");
+            //passengerSurname = Console.ReadLine();
+
+            //Console.Write("İlçe Bilgisi: ");
+            //passengerDistrict = Console.ReadLine();
+
+            //Console.Write("Şehir Bilgisi: ");
+            //passengerCity = Console.ReadLine();
+
+            //Console.Write("Yaş Bilgisi: ");
+            //passengerAge = Console.ReadLine();
+
+            //Console.Write("TC Kimlik Numarası: ");
+            //passengerIdentityNumber = Console.ReadLine();
+
+            //Console.WriteLine();
+
+            //Console.WriteLine();
+            //Console.WriteLine("-----------------");
+            //Console.WriteLine("Yolcu TC  Kimlik No: " + passengerIdentityNumber +" "+ "-Yolcu Bilgileri: " + passengerName +" "+ passengerSurname +" "+ passengerAge +" "+ passengerDistrict +"/"+ passengerCity);
+
+            //Console.Read();
+
+            #endregion
+
+            #region Klavyeden Veri Girişleri Tam Sayı Girişleri ve Dönüşümleri
+
+            //int shoesPrice, computerPrice, chairPrice, tvPrice;
+            //shoesPrice = 1000;
+            //computerPrice = 5000;
+            //chairPrice = 200;
+            //tvPrice = 3000;
+
+            //int shoesCount, computerCount, chairCount, tvCount;
+
+            //Console.Write("Ayakkabı Adet: ");
+            //shoesCount = int.Parse(Console.ReadLine());
+            ////shoesCount = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Bilgisayar Adet: ");
+            //computerCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Sandalye Adet: ");
+            //chairCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Televizyon Adet: ");
+            //tvCount = int.Parse(Console.ReadLine());
+
+            //int totalPrice = (shoesPrice * shoesCount) + (computerPrice * computerCount) + (chairPrice * chairCount) + (tvPrice * tvCount);
+
+            //Console.WriteLine();
+            //Console.WriteLine("Toplam Tutar: "+totalPrice);
+            #endregion
+
+            #region Klavyeden Ondalık Sayı İşlemleri
+
+            //double exam1, exam2, exam3, result;
+
+            //Console.Write("1. Sınav Notu: ");
+            //exam1 = double.Parse(Console.ReadLine());
+
+            //Console.Write("2. Sınav Notu: ");
+            //exam2 = double.Parse(Console.ReadLine());
+
+            //Console.Write("3. Sınav Notu: ");
+            //exam3 = double.Parse(Console.ReadLine());
+
+            //result = (exam1 + exam2 + exam3) / 3;
+
+            //Console.WriteLine();
+            //Console.WriteLine("Sınav Ortalamanız: "+result);
+
+            #endregion
+
+            #region Klavyeden Karakter Girişleri
+
+            //char gender;
+            //Console.Write("Lütfen Cinsiyet Seçiniz: ");
+            //gender = char.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Seçtiğiniz Cinsiyet: "+gender);
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
